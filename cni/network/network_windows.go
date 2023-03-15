c325121f7a47bba34df7c45258d25f4df07da54c
Jaeryn <13284103+jaer-tsun@users.noreply.github.com>
2023-03-15 13:00:58 -0700

    fix: Windows CNI Overlay Gateway Bug (#1849)
    * fix overlay gatway bug that prevented cloud-node-manager-windows from initializing node because it couldn't reach IMDS
    
    * add overlay invoker cns test
    
    * use parseip instead of mask
    
    * scope CNI overlay gateway bug fix to windows only
    
    * update test
    
    * adding UT to verify overlay NAT info is empty
    
    * add podsubnet netInfo test
    
    ---------
    
    Co-authored-by: Jaeryn <tsun.chu@microsoft.com>
    (cherry picked from commit dac5b3133b7d2ba928f74f95ed4806f2180c1673)

--- cni/network/network_windows.go
+++ cni/network/network_windows.go
@@ -381,15 +381,15 @@ func determineWinVer() {
 	}
 }
 
-func getNATInfo(executionMode string, ncPrimaryIPIface interface{}, multitenancy, enableSnatForDNS bool) (natInfo []policy.NATInfo) {
-	if executionMode == string(util.V4Swift) {
+func getNATInfo(nwCfg *cni.NetworkConfig, ncPrimaryIPIface interface{}, enableSnatForDNS bool) (natInfo []policy.NATInfo) {
+	if nwCfg.ExecutionMode == string(util.V4Swift) && nwCfg.IPAM.Mode != string(util.V4Overlay) {
 		ncPrimaryIP := ""
 		if ncPrimaryIPIface != nil {
 			ncPrimaryIP = ncPrimaryIPIface.(string)
 		}
 
 		natInfo = append(natInfo, []policy.NATInfo{{VirtualIP: ncPrimaryIP, Destinations: []string{networkutils.AzureDNS}}, {Destinations: []string{networkutils.AzureIMDS}}}...)
-	} else if multitenancy && enableSnatForDNS {
+	} else if nwCfg.MultiTenancy && enableSnatForDNS {
 		natInfo = append(natInfo, policy.NATInfo{Destinations: []string{networkutils.AzureDNS}})
 	}
 
